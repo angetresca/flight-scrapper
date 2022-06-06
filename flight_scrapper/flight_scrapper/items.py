68b7297e17b62764098351cde9a33f15093a012a
Ange <angetresca@gmail.com>
2022-06-05 22:57:57 -0300

    fix: dates

--- flight_scrapper/flight_scrapper/items.py
+++ flight_scrapper/flight_scrapper/items.py
@@ -9,8 +9,8 @@ import scrapy
 class FlightScrapperItem(scrapy.Item):
     # define the fields for your item here like:
     route = scrapy.Field()
-    departure_date = scrapy.Field()
-    return_date = scrapy.Field()
+    day = scrapy.Field()
+    is_best_offer = scrapy.Field()
     total_duration = scrapy.Field()
     offer_id = scrapy.Field()
     seats_available = scrapy.Field()
--- flight_scrapper/flight_scrapper/spiders/aerolineas_arg.py
+++ flight_scrapper/flight_scrapper/spiders/aerolineas_arg.py
@@ -10,28 +10,28 @@ from .urls import AA_URLS
 
 load_dotenv()
 
-logger = logging.getLogger('AerolineasLogger')
+logger = logging.getLogger("AerolineasLogger")
 
 
 class AerolineasArgSpider(scrapy.Spider):
-    name = 'aerolineas_arg'
+    name = "aerolineas_arg"
     req_number = 0
     headers = {
-                "Authorization": os.getenv("AEROLINEAS_TOKEN"),
-                "accept": "application/json, text/plain, */*",
-                "accept-encoding": "gzip, deflate, br",
-                "accept-language": "es-AR",
-                "origin": "https://www.aerolineas.com.ar",
-                "referer": "https://www.aerolineas.com.ar/",
-                "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": "macOS",
-                "sec-fetch-dest": "empty",
-                "sec-fetch-mode": "cors",
-                "sec-fetch-site": "same-site",
-                "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36",
-                "x-channel-id": "WEB_AR",
-                }
+        "Authorization": os.getenv("AEROLINEAS_TOKEN"),
+        "accept": "application/json, text/plain, */*",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "es-AR",
+        "origin": "https://www.aerolineas.com.ar",
+        "referer": "https://www.aerolineas.com.ar/",
+        "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "macOS",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36",
+        "x-channel-id": "WEB_AR",
+    }
 
     def start_requests(self):
         urls = AA_URLS
@@ -59,7 +59,33 @@ class AerolineasArgSpider(scrapy.Spider):
         departure_date = departure_date.split("-")[2]
         return_date = return_date.split("-")[2]
 
+        # Get only the day from the date
+        departure_date = departure_date[-2:]
+        return_date = return_date[-2:]
+
         return [departure_date, return_date]
+
+    def create_item(
+        self,
+        route: str,
+        departure_date: str,
+        return_date: str,
+        is_best_offer: bool,
+        total_duration: str,
+        offer_id: str,
+        seats_available: int,
+        total_fare: int,
+    ) -> FlightScrapperItem:
+        return FlightScrapperItem(
+            route=route,
+            departure_date=departure_date,
+            is_best_offer=is_best_offer,
+            return_date=return_date,
+            total_duration=total_duration,
+            offer_id=offer_id,
+            seats_available=seats_available,
+            total_fare=total_fare,
+        )
 
     def parse(self, response, **kwargs):
         self.req_number += 1
@@ -69,40 +95,48 @@ class AerolineasArgSpider(scrapy.Spider):
         #     f.write(response.text)
 
         departure_date, return_date = self.get_requested_dates(response.request.url)
+
         response = json.loads(response.text)
 
         branded_offers = response.get("brandedOffers")
         routes = response.get("searchMetadata").get("routes")
 
+        if not branded_offers:
+            return
+
         departure_offers = branded_offers.get("0")
         return_offers = branded_offers.get("1")
 
-        for offer in departure_offers:
-            if offer.get("bestOffer", False) is True:
-                total_duration = offer.get("legs")[0].get("totalDuration")
-                lowest_offer = self.get_lowest_offer(offer.get("offers"))
+        if departure_offers is None or return_offers is None:
+            logger.error(
+                "ERROR in response Nº %s: \n departure_offers = %s \n return_offers = %s", self.req_number, departure_offers, return_offers
+            )
+            departure_offers, return_offers = [], []
 
-                item = FlightScrapperItem(
-                    route=routes[0],
-                    departure_date=departure_date,
-                    return_date=return_date,
-                    total_duration=total_duration,
-                    offer_id=lowest_offer.get("offerId"),
-                    seats_available=lowest_offer.get("seatAvailability").get("seats"),
-                    total_fare=lowest_offer.get("fare").get("total")
-                )
-                yield item
+        for offer in departure_offers:
+            total_duration = offer.get("legs")[0].get("totalDuration")
+            lowest_offer = self.get_lowest_offer(offer.get("offers"))
+
+            yield FlightScrapperItem(
+                route=routes[0],
+                day=departure_date,
+                is_best_offer=offer.get("bestOffer", False),
+                total_duration=total_duration,
+                offer_id=lowest_offer.get("offerId"),
+                seats_available=lowest_offer.get("seatAvailability").get("seats"),
+                total_fare=lowest_offer.get("fare").get("total"),
+            )
 
         for offer in return_offers:
-            if offer.get("bestOffer", False) is True:
-                total_duration = offer.get("legs")[0].get("totalDuration")
-                lowest_offer = self.get_lowest_offer(offer.get("offers"))
+            total_duration = offer.get("legs")[0].get("totalDuration")
+            lowest_offer = self.get_lowest_offer(offer.get("offers"))
 
-                item = FlightScrapperItem(
-                    route=routes[1],
-                    total_duration=total_duration,
-                    offer_id=lowest_offer.get("offerId"),
-                    seats_available=lowest_offer.get("seatAvailability").get("seats"),
-                    total_fare=lowest_offer.get("fare").get("total")
-                )
-                yield item
+            yield FlightScrapperItem(
+                route=routes[1],
+                day=return_date,
+                is_best_offer=offer.get("bestOffer", False),
+                total_duration=total_duration,
+                offer_id=lowest_offer.get("offerId"),
+                seats_available=lowest_offer.get("seatAvailability").get("seats"),
+                total_fare=lowest_offer.get("fare").get("total"),
+            )
